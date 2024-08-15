5ef25045cff1ad5f842c94f0941f3153424348d4
kamal <127002108+kamal8080@users.noreply.github.com>
2024-08-15 11:59:27 +0700

    feat(home): Update Kontak View
    + Menampilkan Data Kontak Dari Database

--- app/Controllers/Kontak.php
+++ app/Controllers/Kontak.php
@@ -14,8 +14,10 @@ class Kontak extends BaseController
 
     public function index()
     {
+        $data['data'] = $this->KontakModel->get();
+
         return view('partials/header')
-            . view('Kontak');
+            . view('Kontak',$data);
     }
 
     public function AdminKontak()
